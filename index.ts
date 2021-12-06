4d06caef1f720fba282312adeef3bc1bbc25e341
EmilioC <emicorgul@gmail.com>
2021-12-06 18:51:53 +0100

    Update the .gitignore file

---
+++ index.ts
@@ -0,0 +1,10 @@
+console.log("Hello universe");
+let numero = 15;
+
+function repetir (){
+for (let index = 0; index < numero; index++) {
+    const element = index;
+    console.log (index)
+;}
+}
+repetir();
