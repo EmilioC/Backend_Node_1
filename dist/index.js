43bb9760185382edc60bbf75bec84e5ede8fa985
EmilioC <emicorgul@gmail.com>
2021-12-08 11:37:17 +0100

    Create the class server and install express package

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,10 @@
 "use strict";
 console.log("Hello universe");
-let numero = 15;
-function repetir() {
-    for (let index = 0; index < numero; index++) {
-        const element = index;
-        console.log(index);
-    }
-}
-repetir();
+// let numero = 15;
+// function repetir() {
+//     for (let index = 0; index < numero; index++) {
+//         const element = index;
+//         console.log(index);
+//     }
+// }
+// repetir();
